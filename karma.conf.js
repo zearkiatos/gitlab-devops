644e2c35d890e9b9d4abf76d9472fdc18c4464c3
zearkiatos <caprilespe@outlook.com>
2021-02-28 23:13:29 -0300

    Add gitlab CI config

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flag: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
